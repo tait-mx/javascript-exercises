151235b21c6628adb87a43941165690a5df9de34
Ernesto Romero <60159005+tait-mx@users.noreply.github.com>
2026-01-23 18:23:22 -0600

    Complet exercise 06_repeatString
    Had to create a new var called "repeat" to receive the
    iteration. Otherwise the iteration string += string gets
    exponential.
    
    The definition let repeat = '' must be inside the function
    at the begining and outside the for loop.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
 
+
+
+const repeatString = function (string, num) {
+    if(num >= 0) {
+    let repeat = '';
+    for (let i = 0; i < num; i++) { repeat += string };
+    return repeat;
+    } else {
+        return "ERROR";
+    }
 };
+
+repeatString('hey', -3);
+
 
 // Do not edit below this line
 module.exports = repeatString;
