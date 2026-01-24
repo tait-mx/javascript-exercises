f37028b5b4e6cac30ff2901444284718653f1138
Ernesto Romero <60159005+tait-mx@users.noreply.github.com>
2026-01-23 23:28:45 -0600

    Solve exercise 08_removeFromArray.js

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+ let size = string.length - 1;
+let final = "";
+for (let i = size; i >= 0; --i) {
+    final += string[i];
+}
+return final;
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item) {
+
+let string = String(array).replace(/,/g,"").replace(String(item), "");
+return Array.from(string, x => Number(x));
 };
+// Array.from([1, 2, 3], (x) => x + x);
+removeFromArray([1,2,3,4], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
