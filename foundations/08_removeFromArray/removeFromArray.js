4a9b70aa5b4b86e3ae192d3b1e34bdb45320c4d6
Ernesto Romero <60159005+tait-mx@users.noreply.github.com>
2026-01-24 04:36:28 -0600

    Solve exercises 08 and 09

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,10 +1,8 @@
-const removeFromArray = function(array, item) {
+const removeFromArray = function(array, ...item) {
 
 let string = String(array).replace(/,/g,"").replace(String(item), "");
 return Array.from(string, x => Number(x));
 };
-// Array.from([1, 2, 3], (x) => x + x);
-removeFromArray([1,2,3,4], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+    let sum = 0;
+    if (int1 < int2) {
+        for (let i = int1; i <= int2; i++) {
+            sum = sum + i;
+        }
+    } else {
+        for (let i = int2; i < int1; i++) {
+            sum = sum + i;
+        }
+    }
+    return sum;
+}
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
