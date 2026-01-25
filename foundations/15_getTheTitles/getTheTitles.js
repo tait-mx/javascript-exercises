3be12178dde0ba1306c9f4388312800e7baa4c03
Ernesto Romero <60159005+tait-mx@users.noreply.github.com>
2026-01-25 13:11:50 -0600

    Solve exercise 15
    Learned to use Objects:
    
    // Create object {parameter, "value"}
    
    // Get value from object's parameter
    let objectName = {parameter: "value"};
    objectName.parameter

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ];
 
+const getTheTitles = function(contant) {
+
+    return [books[0].title, books[1].title]
 };
 
 // Do not edit below this line
