e77e5646c9462574a16304bf6dc9fd8d7552f8e2
Ernesto Romero <60159005+tait-mx@users.noreply.github.com>
2026-01-25 08:57:06 -0600

    Solve exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (fiboNum) {
+    secuence = [0, 1];
+    fiboNum = +fiboNum;
 
-};
 
-// Do not edit below this line
-module.exports = fibonacci;
+    if (fiboNum > 0) {
+
+        for (let i = 1; i <= fiboNum - 1; i++) {
+            // size = secuence.length;
+            secuence.push(secuence[secuence.length - 1] + secuence[secuence.length - 2]);
+        };
+        return secuence.findLast(x => x);
+    } else if (fiboNum == 0) { return 0}
+    else { return "OOPS"}
+}
+    // Do not edit below this line
+    module.exports = fibonacci;
