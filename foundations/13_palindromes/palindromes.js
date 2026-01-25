b1f1502d06d9f1cdf06477dee10e5dd64e103dc0
Ernesto Romero <60159005+tait-mx@users.noreply.github.com>
2026-01-25 06:56:32 -0600

    Solve exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let clean = word.replace(/[\p{P}\s]/gu, "");
+    let reverse = clean.split("").reverse().join("");
+    return clean.toLowerCase() == reverse.toLowerCase();
 };
 
 // Do not edit below this line
