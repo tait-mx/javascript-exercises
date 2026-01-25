e5859e8d92c94415ee0f10b3e48b48c7915c20b4
Ernesto Romero <60159005+tait-mx@users.noreply.github.com>
2026-01-24 23:57:27 -0600

    Solved exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return (num1 + num2)
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return (num1 - num2)
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (i = 0; i <= array.length -1; ++i) {
+    sum = sum + array[i]
+  }
+	return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let mul = array[0];
+  for (i = 1; i <= array.length -1; ++i) {
+    mul = mul * array[i]
+  }
+	return mul;
 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let resultado = 1;
+  for (let i = 1; i <= num; ++i) {
+    resultado = resultado * i;
+  }
+	return resultado;
 };
 
 // Do not edit below this line
