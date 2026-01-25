368a5da61bbb0a406d7d3ec6c06d40fdd22e2401
Ernesto Romero <60159005+tait-mx@users.noreply.github.com>
2026-01-24 22:47:11 -0600

    Solve exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  return +((5/9) * (degrees - 32)).toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  return +((degrees * (9/5)) + 32).toFixed(1)
 };
 
 // Do not edit below this line
