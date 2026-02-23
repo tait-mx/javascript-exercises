3f3681cc36fdf6b7a8043774774196f96e9fd8c6
Ernesto Romero <60159005+tait-mx@users.noreply.github.com>
2026-02-23 01:46:49 -0600

    Solve exercise 10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function(year) {
-return (year%4 == 0 && (year%100 != 0 || year%400 == 0))
-};
+// divisible by four 
+// not divisible by 100 unless divisible by 400
+function leapYears(number) {
+   if (number%4 == 0 && (number%100 != 0 || number%400 == 0)) {return true} else {
+    return false}
+}
+
+console.log(leapYears(1600))
 
 // Do not edit below this line
 module.exports = leapYears;
