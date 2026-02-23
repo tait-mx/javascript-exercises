a003982332751dc9b16c9f202c2915a68cbc0322
Ernesto Romero <60159005+tait-mx@users.noreply.github.com>
2026-02-23 00:32:29 -0600

    Solve 08_removeFromArray

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -4,7 +4,7 @@ function reverseString(string) {
 return string.split("").reverse().join("");
 }
 
-console.log(reverseString("123! abc! Hello, Odinite."))
+console.log(reverseString("hola"))
 
 // Do not edit below this line
 module.exports = reverseString;
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,8 +1,20 @@
-const removeFromArray = function(array, ...item) {
+function removeFromArray(...msg) {
+    let array = msg.at(0);
+    let size = msg.length;
+    let index;
 
-let string = String(array).replace(/,/g,"").replace(String(item), "");
-return Array.from(string, x => Number(x));
-};
+    for (let i = 1; i < size; i++) {
+        while (array.includes(msg.at(i))) {
+        index = array.indexOf(msg.at(i));
+        array.splice(index, 1);
+}
+    }
+
+    return (array)
+}
+console.log(removeFromArray([1, 4, 4, 2, 3], 3, 4))
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
