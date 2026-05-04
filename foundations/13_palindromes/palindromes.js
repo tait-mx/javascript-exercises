cc33667f9f2bbf3684166781321e29e0392f9d44
Ernesto Romero <ernestoromero@Mac-mini.local>
2026-05-04 00:10:18 -0600

    Solved 13 palindromes exercise (truly this time)
    Last commit was wrong because it said 12  and 13 solved
    but it was only 12

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let clean = string.toLowerCase().match(/\w/gi).join("");
+    return clean == clean.split("").reverse().join("")
 };
 
 // Do not edit below this line
