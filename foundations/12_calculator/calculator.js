8973558ea3046743e36c240760066b03e4658fd6
Ernesto Romero <ernestoromero@Mac-mini.local>
2026-05-03 18:39:04 -0600

    Solve 12 and 13

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((a, b) =>  a + b, 0);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+	return array.reduce((a, b) =>  a * b);
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let fact = 1;
+  for(let i = 1; i <= a; i++) {
+    fact = fact * i;
+  };
+
+  if(a == 0) {return 1} else {return fact}
 };
 
 // Do not edit below this line
