6473a2dfcdbde8e69ebd067a14a7e2c1b0019f8b
Ernesto Romero <ernestoromero@Mac-mini.local>
2026-05-04 02:39:42 -0600

    Correct ghost comment symbols

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -2,9 +2,7 @@ const findTheOldest = function(array) {
     array.forEach(a => {if(a.yearOfDeath == undefined) {
         a.yearOfDeath = new Date().getFullYear()
     }});
-    let ages = array.map(a => 
-        // 
-         a.yearOfDeath - a.yearOfBirth);
+    let ages = array.map(a => a.yearOfDeath - a.yearOfBirth);
         let oldest = ages.indexOf(Math.max(...ages));
     return array[oldest];
 };
