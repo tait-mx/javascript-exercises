454137b3259e90bdee0a849a8f4d25079f7c5d69
Ernesto Romero <ernestoromero@Mac-mini.local>
2026-05-04 01:09:02 -0600

    Solved fibo

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let fibo1 = 0;
+    let fibo2 = 1;
+    let fibo3;
+    n = +n;
+    
+    if (n < 0) {fibo3 = "OOPS"};
+    if (n == 0) {fibo3 = 0}
+    if(n == 1) {fibo3 = 1} else {
+    for(let i = 1; i < n; i++) {
+        fibo3 = fibo1 + fibo2
+        fibo1 = fibo2;
+        fibo2 = fibo3
+    }
+}
+    return fibo3
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
