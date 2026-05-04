41b6ceb7ce6ba77b172aae6ad515dacd66c635b8
Ernesto Romero <ernestoromero@Mac-mini.local>
2026-05-04 02:35:57 -0600

    Solve 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.forEach(a => {if(a.yearOfDeath == undefined) {
+        a.yearOfDeath = new Date().getFullYear()
+    }});
+    let ages = array.map(a => 
+        // 
+         a.yearOfDeath - a.yearOfBirth);
+        let oldest = ages.indexOf(Math.max(...ages));
+    return array[oldest];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+        // if(a.yearOfDeath == undefined) {a.yearOfDeath = new Date().getFullYear()} 
